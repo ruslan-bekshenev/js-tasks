39cf3463deec89c6290bfeb839905bd1a9aa4932
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-08-31 23:40:12 +0500

    chooseBestSum

---
+++ 5kyu/chooseBestSum/index.js
@@ -0,0 +1,6 @@
+const chooseBestSum = (t, k, ls) =>
+  ls.reduce((pre, val) => [...pre, ...pre.filter(val => val.length < k).map(v => [...v, val])], [[]])
+    .filter(val => val.length === k)
+    .map(val => val.reduce((pre, val) => pre + val))
+    .filter(val => val <= t)
+    .sort((a, b) => a - b).pop() || null;
